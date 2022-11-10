c598429b8a5b34905096d32c13ae239d6c177b4f
Amal V Prakash <71067360+amalvprakash@users.noreply.github.com>
2022-11-11 00:28:00 +0530

    Delete TestMedicine.java

--- Work_Out-2/Program 4/TestMedicine.java
+++
@@ -1,8 +0,0 @@
-class TestMedicine{
-    public static void main(String[] args) {
-        Medicine m = new Medicine();
-        Tablet t = new Tablet();
-        m.displayLabel();
-        t.displayLabel();
-    }
-}
